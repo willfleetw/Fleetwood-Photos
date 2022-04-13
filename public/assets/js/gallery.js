79460b56bab1c8c1d1e41b35bb272389044d6836
Will Fleetwood <willfleetw@gmail.com>
2022-04-13 12:03:52 -0700

    Update gallery.js to use jquery

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -1,9 +1,38 @@
-window.onload = function () {
-  var gallery = document.getElementById("gallery");
+$(loadGallery());
 
-  console.log(fetch("https://fleetwood.photos/images/photos.json")
-    .then(response => {
-      return response.json();
-    }));
+async function loadGallery() {
+  var photos = await $.get('images/photos.json');
+  var gallery = $('#gallery > .row');
+  console.log(gallery);
 
-};
+  photos.forEach(photo => {
+    var frame = $('<div>', {
+      'class': 'col-4 col-6-medium col-12-small'
+    });
+    
+    var link = $('<a>', {
+      href: 'images/originals/' + photo['name'],
+      'class': 'image fit',
+      'data-lightbox': 'mygallery'
+    });
+    
+    var hasTitle = 'title' in photo;
+    var hasDesc = 'description' in photo;
+    if (hasTitle || hasDesc) {
+      var comment = (hasTitle ? photo['title'] : '') 
+          + (hasTitle && hasDesc ? ' - ' : '') 
+          + (hasDesc ? photo['description'] : '');
+      
+      link.attr('data-title', comment);
+    }
+    
+    var thumbnail = $('<img>', {
+      src: 'images/thumbnails/' + photo['name']
+    });
+
+    link.append(thumbnail);
+    frame.append(link);
+
+    gallery.append(frame);
+  });
+}
