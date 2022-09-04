7ba417b304062b930b1b049e70b14bce2af6f10d
Will Fleetwood <willfleetw@gmail.com>
2022-09-04 10:08:15 -0700

    Fix heights

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -9,13 +9,13 @@ $(window).on('load', function () {
     databaseURL: 'https://fleetwood-photos-default-rtdb.firebaseio.com',
     appId: '1:1059550382284:web:b0d1f58561a6ac0d4a9a69'
   };
-
+  
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const dbRef = ref(getDatabase(app));
-
+  
   var grid = $('.grid');
-
+  
   get(query(dbRef, orderByChild('priority'))).then((snapshot) => {
     snapshot.forEach((child) => {
       var miniURL = 'https://firebasestorage.googleapis.com/v0/b/fleetwood-photos.appspot.com/o/images%2Fmini%2F' + child.key + '.jpg?alt=media'
@@ -23,7 +23,11 @@ $(window).on('load', function () {
       var largeURL = 'https://firebasestorage.googleapis.com/v0/b/fleetwood-photos.appspot.com/o/images%2Flarge%2F' + child.key + '.jpg?alt=media'
       var captionSuffix = " - <a download target='_blank' href='" + smallURL + "'>Small File</a> and <a download target='_blank' href='"+ largeURL + "'>Large File</a>"
       
+      var imgData = child.val();
       var divClass = 'grid-item';
+      if (imgData.height > imgData.width) {
+        divClass += ' grid-item--height2';
+      }
       var item = $('<div>', {
         'class': divClass,
       });
@@ -40,15 +44,15 @@ $(window).on('load', function () {
       imgWrapper.append(img);
       item.append(imgWrapper);
       grid.prepend(item); // prepend since firebase returns ascending order, and we want higher priority shown first
-    });
-    var $grid = $('.grid').imagesLoaded( function() {
-      // init Masonry after all images have loaded
-      $grid.masonry({
-        itemSelector: '.grid-item',
-        // use element for option
-        columnWidth: '.grid-sizer',
-        gutter: 6,
-        percentPosition: true
+      var $grid = $('.grid').imagesLoaded( function() {
+        // init Masonry after all images have loaded
+        $grid.masonry({
+          itemSelector: '.grid-item',
+          // use element for option
+          columnWidth: '.grid-sizer',
+          gutter: 6,
+          percentPosition: true
+        });
       });
     });
   }).catch((error) => {
